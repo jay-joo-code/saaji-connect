09f9073f733904b3cf5619f9fdd93bdfe9e74285
EC2 Default User <ec2-user@ip-172-31-23-32.ap-southeast-1.compute.internal>
2019-10-25 13:11:48 +0000

    image resizing

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { connect } from 'react-redux';
 // import { increment } from 'src/redux/actions/counter';
 import './Home.scss'
-import HomeImage from './home-image.jpg';
+import HomeImage from './resized-landing-80.jpg';
 import CodeImage from './code.jpg';
 import SaajiConnect from './SaajiConnect.bat';
 
