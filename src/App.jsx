8e7c5d5bf23dc1e751ab78fdbe6af7727f95b028
EC2 Default User <ec2-user@ip-172-31-38-22.ap-northeast-1.compute.internal>
2020-01-26 14:17:45 +0000

    ie alert

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,13 @@
 import React from 'react';
 import Home from 'src/components/Home';
+import is from 'is_js';
 
 const App = () => {
+  if (is.ie()) {
+    alert('이 사이트는 인터넷 익스플로러에서 작동하지 않습니다. 크롬을 사용해주세요!');
+    return <div />;
+  }
+  
   return <Home />
 }
 
