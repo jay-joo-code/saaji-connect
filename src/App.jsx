e1341f9966de6c5d513539ab95278d9aefc1985a
EC2 Default User <ec2-user@ip-172-31-38-22.ap-northeast-1.compute.internal>
2020-02-01 09:06:59 +0000

    analytics

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,14 @@ import React from 'react';
 import Home from 'src/components/Home';
 import is from 'is_js';
 import 'src/styles/normalise.css';
+import firebase from 'src/util/firebase';
 
 const App = () => {
   if (is.ie()) {
     alert('이 사이트는 인터넷 익스플로러에서 작동하지 않습니다. 크롬을 사용해주세요!');
     return <div />;
   }
-  
+  firebase.analytics();
   return <Home />
 }
 
