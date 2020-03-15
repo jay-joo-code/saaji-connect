8c06bde3cec5ea8263d4d52debf0109d102fb373
EC2 Default User <email@example.com>
2020-03-15 08:54:51 +0000

    removed console logs

--- client/src/util/api.js
+++ client/src/util/api.js
@@ -3,12 +3,8 @@ import axios from 'axios';
 let URL = '/api';
 
 if (process.env.NODE_ENV !== 'development') {
-  console.log('env is not development. set custom api url')
-  console.log('process.env.CUSTOM_API_URL', process.env.REACT_APP_API_URL);
   URL = process.env.REACT_APP_API_URL;
 }
-
-console.log('baseURL: ', URL)
 
 export default axios.create({
   baseURL: URL,
