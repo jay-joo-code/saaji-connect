6a7e70210f243c38975ad566814feebea8debc8a
EC2 Default User <email@example.com>
2020-03-15 07:05:51 +0000

    append REACT_APP_ to env variable names

--- client/src/util/api.js
+++ client/src/util/api.js
@@ -4,8 +4,8 @@ let URL = '/api';
 
 if (process.env.NODE_ENV !== 'development') {
   console.log('env is not development. set custom api url')
-  console.log('process.env.CUSTOM_API_URL', process.env.CUSTOM_API_URL);
-  URL = process.env.CUSTOM_API_URL;
+  console.log('process.env.CUSTOM_API_URL', process.env.REACT_APP_API_URL);
+  URL = process.env.REACT_APP_API_URL;
 }
 
 console.log('baseURL: ', URL)
